6f4b1c786deaf15aa14eddca8124810297fe8c30
Uherhsen <tovaris4uherhsen@yandex.ru>
2020-11-06 18:10:13 +0300

    Some new additions in cycles to solve problems with finding some articles

--- GetDataTME_with_openpyxl.py
+++ GetDataTME_with_openpyxl.py
@@ -10,6 +10,7 @@ from TME_Python_API import product_import_tme
 
 # Функция считает все ячейки первого столбца в которых что то написано,до тех пор пока не встретит пустую ячейку "None"
 def number_of_articles(path):
+    """Функция считает все ячейки первого столбца в которых что то написано,до тех пор пока не встретит пустую ячейку None"""
     # Открываем Эксель
     wb = openpyxl.load_workbook(path)#путь к файлу
     sheet = wb.active
@@ -23,6 +24,7 @@ def number_of_articles(path):
 # Функция создающая список артикулов. Принимает число артикулов и номер колонки в виде буквы (str): 'A'- первая колонка
 #
 def articlesList(n,column,path):
+    """ Функция создающая список артикулов. Принимает число артикулов и номер колонки в виде буквы (str): 'A'- первая колонка"""
     # Открываем Эксель
     wb = openpyxl.load_workbook(path)#путь к файлу
     sheet = wb.active 
@@ -36,8 +38,9 @@ def articlesList(n,column,path):
 # Функция для вывода пинга
 #   
 def ping():
-        ping_data = product_import_tme(token, app_secret, 'Utils/Ping', params={})
-        print(ping_data)
+    """Функция для вывода пинга"""
+    ping_data = product_import_tme(token, app_secret, 'Utils/Ping', params={})
+    print(ping_data)
 #        
 # Функция проставляет оригинальные артикулы производителя, дескрипшен, ссылку на фото, ссылку на страницу продукта и вес в КГ! 
 #            
@@ -48,12 +51,15 @@ def search_articles(articles_list, path, params, token, app_secret,action1, rng1
     wb = openpyxl.load_workbook(path)#путь к файлу
     sheet = wb.active
     rng2=len(articles_list)
-    
+    params2 = params.copy()
+    index = 0
     for j in range(rng1,rng2):
         global flag
-        if flag != 1:
+        if flag != 1: # Флаг нужен для принудительного завершения цикла, меняется из графического интерфейса
             break
         else:
+            index += 1
+            print( "Акт 1", index, 'Поиск базовых параметров')
             params['SearchPlain'] = str(articles_list[j])
             all_data = product_import_tme(token, app_secret, action1, params)
             #print(all_data)
@@ -65,7 +71,7 @@ def search_articles(articles_list, path, params, token, app_secret,action1, rng1
                 if all_data['Data']['ProductList'][0]['Photo'] == "":
                     print('нет фото')
                 else:
-                    sheet['E'+str(j+1)] = "https://" + all_data['Data']['ProductList'][0]['Photo'][2:] # "https://" + 
+                    sheet['E'+str(j+1)] = "https://" + all_data['Data']['ProductList'][0]['Photo'][2:] # добавляем КАРТИНКИ!
                 
                 sheet['F'+str(j+1)] = all_data['Data']['ProductList'][0]['ProductInformationPage'][2:]
                 weight = (all_data['Data']['ProductList'][0]['Weight'])
@@ -74,15 +80,37 @@ def search_articles(articles_list, path, params, token, app_secret,action1, rng1
                     #print('{:f}'.format(weight))
                     weight = round(weight,(('{:f}'.format(weight)).count('0'))+1) # округление
                 sheet['D'+str(j+1)] = weight
-            except IndexError:
+                
+            except IndexError: # ____________________________________________________________________________________________________В этом блоке запускаются другие экшены
                 if all_data["Status"]=="OK":
-                    print('\nСтатус сети ',all_data["Status"],'\nАртикул "',articles_list[j],'" отсутствует на TME\n')
-                    sheet['C'+str(j+1)] = 'Артикула нет на TME'
+                    
+                    params2['SymbolList[0]'] = articles_list[j]
+                    action3 = 'Products/GetProductsFiles'
+                    try:
+                        all_data = product_import_tme(token, app_secret, action3, params2 ) # используем другой экшн для нахождения картинок Products/GetProductsFiles 
+                        
+                    except:
+                        print ('\nСтатус сети ',all_data["Status"],'\nАртикул "',articles_list[j],'" отсутствует на TME\n')
+                        sheet['C'+str(j+1)] = 'Артикула нет на TME'
+                    else:
+                        
+                        sheet['E'+str(j+1)] = "https:" + all_data['Data']['ProductList'][0]['Files']['PhotoList'][0]
+                        sheet['B'+str(j+1)] = articles_list[j] # Копируем во-второй столбец артикул для поиска другими экшенами
+                    try:
+                        action0 = 'Products/GetProducts' # получаем веса по-другому экшену
+                        all_data = product_import_tme(token, app_secret, action0, params2 ) # используем другой экшн для нахождения картинок Products/GetProductsFiles 
+                        weight = (all_data['Data']['ProductList'][0]['Weight'])
+                        if all_data['Data']['ProductList'][0]['WeightUnit']=='g':
+                            weight = weight*0.001
+                            weight = round(weight,(('{:f}'.format(weight)).count('0'))+1) # округление
+                    except:
+                        print ('\nСтатус сети ',all_data["Status"],'\nАртикул "',articles_list[j],'" нет весов\n')
+                    else: # добавляем Веса 2
+                        sheet['D'+str(j+1)] = weight
                 else:
                     print('\nСтатус сети ',all_data["Status"])
-                    sheet['C'+str(j+1)] = all_data['Data']['ProductList'][0]['Description']
                     time.sleep(2) 
-                continue
+                #continue
     wb.save(path)
     #time.sleep(0.1)
     print('\nПоиск базовых параметров завершен')
@@ -97,17 +125,19 @@ def search_param(articles_list,path,params,token,app_secret,action2, rng1=0):
     sheet = wb.active
     rng2=len(articles_list)
     print('\nЦикл проставления параметров\n')
-    
+    index = 0
     for j in range(rng1,rng2):
         global flag
         if flag != 1:
             break
         else:
+            index += 1
+            print( "Акт 2", index, 'Цикл проставления параметров')
             if articles_list[j] != None:
                 params['SymbolList[0]'] = articles_list[j]
                 
                 try:
-                    all_data = product_import_tme(token, app_secret, action2, params) # НЕ РАБОТАЕТ если не найденый артикул, по какой то причине есть во второй колонке !!!
+                    all_data = product_import_tme(token, app_secret, action2, params) # НЕ РАБОТАЕТ если не находящийся на ТМЕ артикул, по какой то причине есть во второй колонке !!!
                 except: # пропускаем принудительный выход raise
                     print(' HTTPError\n_____________________________________________________')
                     continue
@@ -150,12 +180,14 @@ def products_files(articles_list, path,params,token,app_secret,action3, rng1=0):
     sheet = wb.active
     rng2=len(articles_list)
     print('\nЦикл поиска ссылок на даташит\n')
-    
+    index = 0
     for j in range(rng1,rng2):
         global flag
         if flag != 1:
             break
         else:
+            index += 1
+            print( "Акт 3", index, 'Цикл поиска ссылок на даташит')
             if articles_list[j] != None:
                 params['SymbolList[0]'] = articles_list[j]
                 try:
@@ -176,7 +208,13 @@ def products_files(articles_list, path,params,token,app_secret,action3, rng1=0):
                 else:
                     print('ошибка статуса сети')
                     break
-                    
+                
+                try: # Добываем Оригинальный артикул, будем считать что это артикул производителя
+                    OriginalSymbol = product_import_tme(token, app_secret, 'Products/GetProducts', params)
+                    sheet['L'+str(j+1)]= OriginalSymbol['Data']['ProductList'][0]['OriginalSymbol']
+                except: # пропускаем принудительный выход raise
+                    print("Нет оригинального наименования")
+                    continue
             else:
                 print('Нет артикула','\n'+('_'*50))
                 j+=1
@@ -190,7 +228,8 @@ if __name__ == '__main__':
     params={'Country' : 'RU','Language' : 'RU',}
     token = 'ac434c181917ed4e51c49a2027bfd040e9f2da0054be7'
     app_secret = '0b748f6e5d340d693703'
-    action1 = 'Products/Search' # request method, метод пинг Utils/Ping
+    action0 = 'Products/GetProducts' # request method, метод пинг Utils/Ping, Products/Search
+    action1 = 'Products/Search'
     action2 = 'Products/GetParameters'
     action3 = 'Products/GetProductsFiles'
     n = number_of_articles(xlsxpath)
